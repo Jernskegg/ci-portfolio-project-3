c4579d4b1142238d4495dece147bf49cdb0a543a
Jernskegg <casper.hille@outlook.com>
2021-06-02 12:56:32 +0000

    Add Enemyship position, removed number generator

--- run.py
+++ run.py
@@ -3,7 +3,10 @@
 # main
 import random
 
+def enemy_ship():
+    enemy_ship_row = random.randint(1, 10)
+    enemy_ship_col = random.randint(1, 10)
+    enemy_ship_pos = [enemy_ship_row, enemy_ship_col]
+    return enemy_ship_pos
 
-def number_generator():
-    number = random.randint(1, 10)
-    return number
+print(enemy_ship())
