9fc319b41bc22357b20bf671b9d995a5266e1ddb
Jernskegg <casper.hille@outlook.com>
2021-06-02 13:43:08 +0000

    Add game and guess

--- run.py
+++ run.py
@@ -3,10 +3,28 @@
 # main
 import random
 
-def enemy_ship():
-    enemy_ship_row = random.randint(1, 10)
-    enemy_ship_col = random.randint(1, 10)
+
+def enemy_ship(dificulty):
+    enemy_ship_row = random.randint(1, dificulty)
+    enemy_ship_col = random.randint(1, dificulty)
     enemy_ship_pos = [enemy_ship_row, enemy_ship_col]
     return enemy_ship_pos
 
-print(enemy_ship())
+
+def guess():
+    guess_row = input("Make your guess! \nRow: ")
+    guess_col = input("Collumn: ")
+    guessed_answered = [int(guess_row), int(guess_col)]
+    return guessed_answered
+
+
+def game():
+    dificulty = 10
+    enemy_ship_pos = enemy_ship(dificulty)
+    if enemy_ship_pos == guess():
+        print("HIT!")
+    else:
+        print(f"MISS! enemy ship was at {enemy_ship_pos}")
+
+
+game()
